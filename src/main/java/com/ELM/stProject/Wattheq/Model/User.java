aec98f3f52b06e0a921f6e43b58c49e80acf8b23
abdalsahli <abdalsahli@elm.sa>
2020-03-05 16:22:47 +0300

    DTOS and Validation

--- src/main/java/com/ELM/stProject/Wattheq/Model/User.java
+++ src/main/java/com/ELM/stProject/Wattheq/Model/User.java
@@ -1,10 +1,7 @@
 package com.ELM.stProject.Wattheq.Model;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
